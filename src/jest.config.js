fc852663f702adc46e95c905da3bc2cd49e1d72f
oscar <oscar.vasquez@zentro.com.mx>
2023-04-17 18:13:24 -0600

    first commit

---
+++ src/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // verbose:
+    // Indica si cada prueba individual debe reportar 
+    // su resultado durante la ejecución. Independiente de esto todos los 
+    // errores siempre se muestran al final de la ejecución.
+    verbose: true, 
+}
